536dfe416c232a79051690647e0ffc9038a8fdb1
Florida Lottery <lotto@MININT-0DGVP15.dol.state.fl.us>
2020-10-15 09:53:35 -0400

    added web3 sendtransaction

--- js/erc20balance.js
+++ js/erc20balance.js
@@ -22,7 +22,7 @@ const promisify = (inner) =>
 
 async function getBalance() {
     var address, wei, balance
-    address = "0xC5687bD97877F096712D400c92938835d5ef62F8";
+    address = "0x391504b56c35Ef43E68c3dFdC0bB2C80fAb0Dbc7";
     wei = promisify(cb => web3.eth.getBalance(address, cb))
     try {
         balance = web3.fromWei(await wei, 'ether')
@@ -36,26 +36,34 @@ async function getBalance() {
     
 }
 
+async function getCommunityFundBalance() {
+    var address, wei, balance
+    address = "0xb80a7458eec231a1c11be35711522f72511a6a27"; //comm fund address
+    wei = promisify(cb => web3.eth.getBalance(address, cb))
+    try {
+        balance = web3.fromWei(await wei, 'ether')
+        document.getElementById("output3").innerHTML = balance + " ETH";
+        document.getElementById("outputusd").innerHTML = balance + " ETH";
+    } catch (error) {
+        document.getElementById("output3").innerHTML = error;
+        document.getElementById("outputusd").innerHTML = balance + " ETH";
+    }
+
+    
+}
+
 window.addEventListener('load', function() {
     getBalance();
     
 });
 
-// function initWeb3() {
-//     if (typeof web3 !== 'undefined') {
-//         console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
-//         window.web3 = new Web3(web3.currentProvider);
-//     } else {
-//         console.log('No Web3 Detected... using HTTP Provider')
-//         window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/81ad8e62ebbe4002bc9e40fdfbd18b92"));
-//     }
-// }
+
 
 
 async function getERC20Balance() {
     var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
     address = web3.eth.accounts[0];
-    contractAddress = "0xC5687bD97877F096712D400c92938835d5ef62F8"
+    contractAddress = "0x391504b56c35ef43e68c3dfdc0bb2c80fab0dbc7"
     contractABI = human_standard_token_abi
 
     tokenContract = web3.eth.contract(contractABI).at(contractAddress)
@@ -77,6 +85,7 @@ async function getERC20Balance() {
 
 window.addEventListener('load', function() {
     getBalance();
+    getCommunityFundBalance();
 });
 
 
